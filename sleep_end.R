f64558ba2b5d191889c233f7a7e118e745d6ef58
Patrick Kratschmer <Patrick.Kraetschmer@gmail.com>
2017-08-22 22:39:13 +0100

    Update comments

--- sleep_end.R
+++ sleep_end.R
@@ -1,17 +1,19 @@
 # sleep_end calculates the end of each sleep episode
-# In analysR, this function is called on each column of five_min_bouts, 
-# which by itself represents each individual channel used in the experiment. 
-# sleep_end uses a sliding window of length 2 and slides down the input vector until it encounters
-# a transition from `1` to `0`. It will then store the index of the `0` in sleep_end_vec.
-# Note that because we added a ghost_row (consisting of `1`'s) to DAM_raw_clean2 in
-# analysR, the row index of each element in five_min_bouts is shifted downwards by `1`.
-# Hence, to store the time point of the second element of the sliding window, i.e. the `0`,
-# we actually have to store the index of the `1`, which is synonymous with the index
+# In analysR, this function is called on each column of 
+# five_min_bouts, which by itself represents each individual 
+# channel used in the experiment. sleep_end uses a sliding 
+# window of length 2 and slides down the input vector until it encounters
+# a transition from `1` to `0`. It will then store the index of 
+# the `0` in sleep_end_vec. Note that because we added a ghost_row 
+# (consisting of `1`'s) to DAM_raw_clean2 in analysR, the row index 
+# of each element in five_min_bouts is shifted downwards by `1`.
+# Hence, to store the time point of the second element of the 
+# sliding window, i.e. the `0`, we actually have to store the 
+# index of the `1`, which is synonymous with the index
 # of the `0` minus `1`. (see sleep_start.R for more information)
 sleep_end <- function(vec){
     
     adjusted_length <- length(vec) - 1
-    
     sleep_end_vec <- vector(mode = "numeric")
     
     for(i in 1:adjusted_length){
--- sleep_start.R
+++ sleep_start.R
@@ -1,20 +1,29 @@
-# sleep_start calculates when a fly started a period of 5 min of inactivity
-# sleep_start slides a window of length 2 down the input vector: when the window 
-# is positioned over a `0` followed by a `1`, the index of the `1` minus 1
-# is stored in sleep_start_vec. Note that subtracting 1 from the index of the `1` is necessary because 
-# five_minute_bouts starts with a row that is equivalent to 0 minutes; this is due to the fact that
-# we added a ghost of 1's row to DAM_raw_clean2. The ghost row is required to identify whether a fly was sleeping
-# at the very beginning of the experiment (i.e. already sleeping at time = `1` min). The result of this
-# ghost row is that any index we identify as the beginning of sleep will be shifted forward in time by 1 min.
-# Hence, we need to subtract 1 from any index we find. This leads to the fase impression that we are
-# storing the index of the `0` in sleep_start_vec (i.e. `i`); when in fact we are storing the index of
-# `1`, which is (`i + 1 - 1`), which is `i`, and hence identical to the index of `0`. It must be clear though
-# that the physiological entity we are storing in sleep_start_vec is the `1`, i.e. the minute during which the
+# sleep_start calculates when a fly started a period of 5 min
+# of inactivity sleep_start slides a window of length 2 down 
+# the input vector: when the window is positioned over a `0` 
+# followed by a `1`, the index of the `1` minus 1
+# is stored in sleep_start_vec. Note that subtracting 1 
+# from the index of the `1` is necessary because 
+# five_minute_bouts starts with a row that is equivalent 
+# to 0 minutes; this is due to the fact that
+# we added a ghost of 1's row to DAM_raw_clean2. The ghost 
+# row is required to identify whether a fly was sleeping
+# at the very beginning of the experiment (i.e. already 
+# sleeping at time = `1` min). The result of this
+# ghost row is that any index we identify as the beginning 
+# of sleep will be shifted forward in time by 1 min.
+# Hence, we need to subtract 1 from any index we find. 
+# This leads to the fase impression that we are
+# storing the index of the `0` in sleep_start_vec (i.e. `i`); 
+# when in fact we are storing the index of
+# `1`, which is (`i + 1 - 1`), which is `i`, and 
+# hence identical to the index of `0`. It must be clear though
+# that the physiological entity we are storing in 
+# sleep_start_vec is the `1`, i.e. the minute during which the
 # fly started to sleep.
 sleep_start <- function(vec){
     
     adjusted_length <- length(vec) - 1
-    
     sleep_start_vec <- vector(mode = "numeric")
     
     for(i in 1:adjusted_length){
