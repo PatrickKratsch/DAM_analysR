bfe932fcb2089016befc7a5d77a0beafcfaddf39
PK <Patrick.Kraetschmer@gmail.com>
2017-08-23 23:17:50 +0100

    Shorten code a bit

--- sleepstart_vs_boutlength.R
+++ sleepstart_vs_boutlength.R
@@ -1,10 +1,12 @@
 sleepstart_vs_boutlength_txt <- function(analysR_output, xlsx_file_name, channels = 1:32){
     
-    # NB: the xlsx package is built in JAVA, so you need JAVA installed to run this script.
+    # NB: the xlsx package is built in JAVA, 
+    # so you need JAVA installed to run this script.
     library("xlsx")
     library("plyr")
     
-    # Create data frames for each fly of sleep start times vs bout lengths.
+    # Create data frames for each fly 
+    # of sleep start times vs bout lengths.
     # Store these data frames in a list.
     sleepstart_vs_boutlength_dfs <- list()
     i <- 1
@@ -20,8 +22,10 @@ sleepstart_vs_boutlength_txt <- function(analysR_output, xlsx_file_name, channel
         i <- i + 1
     }
     
-    # Add 1's or 0's as a third column to each dataframe in sleepstart_vs_boutlength_dfs.
-    # This way, you know which sleep episode (and corresponding length) happened during
+    # Add 1's or 0's as a third column to each 
+    # dataframe in sleepstart_vs_boutlength_dfs.
+    # This way, you know which sleep episode 
+    # (and corresponding length) happened during
     # the night and which ones during the day.
     light_regime <- analysR_output$light_regime
     i <- 1
@@ -30,18 +34,14 @@ sleepstart_vs_boutlength_txt <- function(analysR_output, xlsx_file_name, channel
         light_regime_current <- vector(mode = "numeric")
         
         j <- 1
-        
         for(bout in df$sleep_start){
             
-            identifier <- light_regime[bout]
-            
-            light_regime_current[j] <- identifier
-            
+            light_regime_current[j] <- light_regime[bout]
             j <- j + 1
         }
         
-        sleepstart_vs_boutlength_dfs[[i]] <- cbind(sleepstart_vs_boutlength_dfs[[i]], light_regime = light_regime_current)
-        
+        sleepstart_vs_boutlength_dfs[[i]] <- cbind(sleepstart_vs_boutlength_dfs[[i]], 
+                                                   light_regime = light_regime_current)
         i <- i + 1
     }
     
